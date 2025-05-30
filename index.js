5b97a596194a7a790d0223a16a81b1700e52fbc4
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-05-30 06:18:12 +0100

    my responsive portfolio

---
+++ index.js
@@ -0,0 +1,5 @@
+// Toggle mobile menu
+document.getElementById('menu-btn').addEventListener('click', function () {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+});
